907d2ef8af9b522506fa012c3337695ff7fdf7b2
privero <65060696+p-rivero@users.noreply.github.com>
2023-07-30 13:44:19 +0200

    Add some positional rules and node count

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,4 +1,5 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
@@ -13,13 +14,23 @@ public class MyBot : IChessBot
     private Move bestMove;
     private int bestScore;
     private readonly int startDepth = 4;
+    private int alphabetaNodes; // #DEBUG
+    private int quiescenceNodes; // #DEBUG
 
     public Move Think(Board board, Timer timer)
     {
         this.board = board;
         bestScore = -999999;
+        alphabetaNodes = 0; // #DEBUG
+        quiescenceNodes = 0; // #DEBUG
 
-        AlphaBetaSearch(startDepth, -999999, 999999);
+        int score = AlphaBetaSearch(startDepth, -999999, 999999);
+
+        Console.WriteLine("{0} (score = {1})", bestMove, score); // #DEBUG
+        Console.WriteLine("Nodes:        {0}", alphabetaNodes + quiescenceNodes); // #DEBUG
+        Console.WriteLine("  AlphaBeta:  {0}", alphabetaNodes); // #DEBUG
+        Console.WriteLine("  Quiescence: {0}", quiescenceNodes); // #DEBUG
+        Console.WriteLine(); // #DEBUG
 
         return bestMove;
     }
@@ -30,6 +41,8 @@ public class MyBot : IChessBot
         {
             return QuiescenceSearch(alpha, beta);
         }
+        
+        alphabetaNodes++; // #DEBUG
         
         if (board.IsInCheckmate())
         {
@@ -68,6 +81,7 @@ public class MyBot : IChessBot
 
     private int QuiescenceSearch(int alpha, int beta)
     {
+        quiescenceNodes++; // #DEBUG
         int standScore = TurochampEvaluate();
 
         if (standScore >= beta)
@@ -102,29 +116,87 @@ public class MyBot : IChessBot
 
     private int TurochampEvaluate()
     {
-        int scoreCp = 0;
-        
         var AddMaterialScoreForColor = (bool whiteColor, int multiplier) =>
         {
-            var AddMaterialScoreForPiece = (PieceType pieceType, int score) => 
-                scoreCp += board.GetPieceList(pieceType, whiteColor).Count * score * multiplier;
-            AddMaterialScoreForPiece(PAWN, 100);
-            AddMaterialScoreForPiece(KNIGHT, 300);
-            AddMaterialScoreForPiece(BISHOP, 350);
-            AddMaterialScoreForPiece(ROOK, 500);
-            AddMaterialScoreForPiece(QUEEN, 1000);
+            var AddMaterialScoreForPiece = (PieceType pieceType, int score) => board.GetPieceList(pieceType, whiteColor).Count * score * multiplier;
+            return AddMaterialScoreForPiece(PAWN, 100)
+                + AddMaterialScoreForPiece(KNIGHT, 300)
+                + AddMaterialScoreForPiece(BISHOP, 350)
+                + AddMaterialScoreForPiece(ROOK, 500)
+                + AddMaterialScoreForPiece(QUEEN, 1000);
         };
 
-        var AddPositionalScoreForColor = (bool whiteColor, int multiplier) =>
+        var AddPositionalScoreForCurrentPlayer = () =>
         {
-            
+            int positionalScore = 0;
+            var nonPawnDefenders = NumberOfNonPawnDefenders();
+            var pawnDefenders = NumberOfPawnDefenders();
+
+			// Mobility score (rules 1, 3): use the fact that moves are grouped by piece
+            int currentPieceIndex = -1;
+			int currentMoveCount = 0;
+            var FlushMobilityScore = () => positionalScore += (int)Math.Sqrt(10000 * currentMoveCount); // 100 * sqrt(numMoves)
+            foreach (Move move in board.GetLegalMoves())
+			{
+                if (move.MovePieceType == PAWN || move.IsCastles)
+				{
+                    continue;
+				}
+				int fromIndex = move.StartSquare.Index;
+                if (fromIndex != currentPieceIndex && currentPieceIndex != -1)
+				{
+                    FlushMobilityScore();
+                    currentMoveCount = 0;
+                }
+                currentMoveCount += move.IsCapture ? 2 : 1;
+                currentPieceIndex = fromIndex;
+            }
+            FlushMobilityScore();
+
+            // Piece safety (rule 2)
+            var AddPieceSafetyScoreNonPawn = (PieceType pieceType) =>
+            {
+                foreach (var piece in board.GetPieceList(pieceType, board.IsWhiteToMove))
+                {
+                    int index = piece.Square.Index;
+                    int defenders = nonPawnDefenders[index] + pawnDefenders[index];
+                    positionalScore += defenders > 1 ? 150 : defenders > 0 ? 100 : 0; // 1 point if defended, 1.5 points if defended 2+ times
+                }
+            };
+            AddPieceSafetyScoreNonPawn(ROOK);
+            AddPieceSafetyScoreNonPawn(BISHOP);
+            AddPieceSafetyScoreNonPawn(KNIGHT);
+
+            // Pawn credit (rule 6)
+            foreach (var piece in board.GetPieceList(PAWN, board.IsWhiteToMove))
+            {
+                Square square = piece.Square;
+                positionalScore += (board.IsWhiteToMove ? square.Rank - 1 : 6 - square.Rank) * 20; // 0.2 points for each rank advanced
+                positionalScore += nonPawnDefenders[square.Index] > 0 ? 30 : 0; // 0.3 points if defended by a non-pawn
+            }
+
+            return positionalScore;
         };
 
         int whiteMultiplier = board.IsWhiteToMove ? 1 : -1;
-        AddMaterialScoreForColor(true, whiteMultiplier);
-        AddPositionalScoreForColor(true, whiteMultiplier);
-        AddMaterialScoreForColor(false, -whiteMultiplier);
-        AddPositionalScoreForColor(false, -whiteMultiplier);
+        int scoreCp = AddMaterialScoreForColor(true, whiteMultiplier)
+            + AddMaterialScoreForColor(false, -whiteMultiplier)
+            + AddPositionalScoreForCurrentPlayer();
+        board.ForceSkipTurn();
+        scoreCp -= AddPositionalScoreForCurrentPlayer();
+        board.UndoSkipTurn();
         return scoreCp;
+    }
+
+    private int[] NumberOfNonPawnDefenders()
+	{
+        // TODO
+        return new int[64];
+	}
+
+    private int[] NumberOfPawnDefenders()
+    {
+        // TODO
+        return new int[64];
     }
 }
